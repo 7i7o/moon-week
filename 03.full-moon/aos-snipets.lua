fe5a1ab7f1c54d5be6e6fb8a6d22a23c3c014f95
7i7o <mparij@gmail.com>
2023-11-17 15:26:09 -0300

    add an aos-cli checkInbox function

---
+++ 03.full-moon/aos-snipets.lua
@@ -0,0 +1,2 @@
+-- function for AOS-CLI to pretty print the messages in the inbox
+function checkInbox() local o = ''; for i,m in ipairs(inbox) do for i,t in ipairs(m.tags) do if t.name == 'body' then o = o .. t.value .. '\n---\n' end end end; return o end
