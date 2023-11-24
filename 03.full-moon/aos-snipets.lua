79930e4149d8054682f83ea7c08700aa0340eb09
7i7o <mparij@gmail.com>
2023-11-24 11:38:15 -0300

    avoid linter warnings

--- 03.full-moon/aos-snipets.lua
+++ 03.full-moon/aos-snipets.lua
@@ -1,2 +1,3 @@
 -- function for AOS-CLI to pretty print the messages in the inbox
-function checkInbox() local o = ''; for i,m in ipairs(inbox) do for i,t in ipairs(m.tags) do if t.name == 'body' then o = o .. t.value .. '\n---\n' end end end; return o end
+local inbox -- just to avoid linter warning
+function CheckInbox() local o = ''; for i,m in ipairs(inbox) do for i,t in ipairs(m.tags) do if t.name == 'body' then o = o .. t.value .. '\n---\n' end end end; return o end
